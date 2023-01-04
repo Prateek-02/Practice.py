3b431ab629a6b6fbe78c729748d7552d3dbbe1e8
Prateek <Prateekrajgrd74@gmail.com>
2023-01-04 23:21:25 +0530

    forloops.py

--- forloops.py
+++ forloops.py
@@ -1,10 +1,14 @@
-for i in range(0,101,2):
-    if i == 20:
-        continue;
-    print(i,end=" ")
+# for i in range(0,101,2):                 #range(start,end,step)   here the step argument is 2,this means it will print like - 0 2 4 6 8
+#     if i == 20:
+#         continue;                        #it will skip 20 and print all the numbers in range
+#     print(i,end=" ")
 
 # colors = ["red","green","blue","yellow"]
 # for color in colors:
 #     print(color)
 #     for i in color:
 #         print(i)
+
+
+for i in range(0,10):
+    print(i**2)
