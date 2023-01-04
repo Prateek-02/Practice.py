0c3310d5d830ee266e6d156e07deb9918bf22614
Prateek <Prateekrajgrd74@gmail.com>
2023-01-04 21:59:30 +0530

    if-elif-else.py

---
+++ if-elif-else.py
@@ -0,0 +1,31 @@
+#Ques 1
+# a = int(input("Enter your age: "))
+# print("Your age is: ",a)
+
+# if a>18:
+#     print("you can drive")
+# else:
+#     print("you can't drive")    
+   
+    
+#Ques2
+# applePrice = 210
+# budget = 200
+# if applePrice <= budget:
+#     print('Alexa, add 1 kg apple to cart.')    
+# else:
+#     print("Alexa, don't add Apple to cart")  
+
+  
+#Ques3
+num = int(input("enter the value: "))
+if(num>0):
+    print("num is positive")
+elif(num==0):
+    print("num is Zero") 
+elif(num==999):
+    print("nunm is special")    
+else:
+    print("num is negative")       
+
+    
