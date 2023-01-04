9045d6e6f3a61e8df6402b53278216e6e5deac21
Prateek <Prateekrajgrd74@gmail.com>
2023-01-04 22:45:01 +0530

    forloops.py

---
+++ forloops.py
@@ -0,0 +1,10 @@
+for i in range(0,101,2):
+    if i == 20:
+        continue;
+    print(i,end=" ")
+
+# colors = ["red","green","blue","yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
