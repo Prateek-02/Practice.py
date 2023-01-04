c1a83a747f97d7ff6962166ca03c29d80d275529
Prateek <Prateekrajgrd74@gmail.com>
2023-01-04 23:20:55 +0530

    whileloops.py

---
+++ whileloops.py
@@ -0,0 +1,16 @@
+# i = int(input("enter the num: "))
+# while i <= 50:
+#     i = int(input("enter the num: "))
+#     print(i)
+# print("Done with loop")    
+    
+
+
+i = 5
+while i > 0:
+    print(i)
+    i = i-1
+else:
+    print("Done with loop")    
+
+    
