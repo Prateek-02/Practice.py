fb0f81d08632719c7f872bbb12fc9f7985cdf68d
Prateek <Prateekrajgrd74@gmail.com>
2023-01-04 21:27:14 +0530

    strmethids.py

---
+++ strmethods.py
@@ -0,0 +1,24 @@
+a = "Harry!!!! !!!!! Harry!"
+print(a.upper())
+print(a.lower())
+print(a.strip("!"))
+print(len(a))
+print(a.replace("Harry","John"))
+print(a.split(" "))
+print(a.count("Harry"))
+
+b = "intro to js"
+print(b.capitalize())
+
+c = "Welcome to the console"
+print(len(c))
+print(len(c.center(50)))
+
+d = "Welcome to comnsole!!"
+print(d.endswith("!"))
+
+e = "He's name is Dan. He is an honest man."
+print(e.find("is"))
+
+f = "Welcome"
+print(f.isalpha())
