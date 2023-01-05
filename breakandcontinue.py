5d237430dda5e977dbdea8ef5effb6251ea3fe13
Prateek <Prateekrajgrd74@gmail.com>
2023-01-05 08:26:35 +0530

    breakandcontinue.py

--- breakandcontinue.py
+++ breakandcontinue.py
@@ -1,16 +1,16 @@
-#students = ["ram", "shyam", "kishan", "radha", "radhika"]
-
-#for student in students:
-#    if student == "kishan":
-#       continue;
-#    print(student)
-    
 students = ["ram", "shyam", "kishan", "radha", "radhika"]
 
 for student in students:
-    if student == "kishan":
-        break;
-    print(student)
+   if student == "kishan":
+      continue;
+   print(student)
+    
+# students = ["ram", "shyam", "kishan", "radha", "radhika"]
+
+# for student in students:
+#     if student == "kishan":
+#         break;
+#     print(student)
     
         
 
