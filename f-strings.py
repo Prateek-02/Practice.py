5210b64b3d5daa525d1dec2a66b8a69bf3b1c533
Prateek <Prateekrajgrd74@gmail.com>
2023-01-06 20:22:00 +0530

    f-strings.py

---
+++ f-strings.py
@@ -0,0 +1,9 @@
+letter = "Hey my name is {} and i am from {}"
+country = "India"
+name = "Harry"
+
+print(letter.format(name,country))
+print(f"Hey my name is {name} and i am from {country}")
+
+price = 49.0999
+print(f"for only {price:.2f} dollars you can buy it")
