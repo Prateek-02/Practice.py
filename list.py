d86ceb708a42aaf1909dc6e7f48fe497ce2020f8
Prateek <Prateekrajgrd74@gmail.com>
2023-01-06 09:08:37 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,20 @@
+# marks = [3,5,6,"Harry",True,6,7,2,32,345,23]
+
+# if 7 in marks:
+#     print("Yes")
+# else:
+#     print("No")    
+    
+# if "arry" in "harry":
+#     print("Yes")    
+
+# print(marks[1:9:2]) 
+# print(marks[::-1])
+
+
+
+lst = [i for i in range(4)]
+print(lst)
+
+lst = [i*i for i in range(10) if i%2==0]
+print(lst)
