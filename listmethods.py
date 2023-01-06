3bf2df243e8ce7a50e5cd745e34df68ac79ab72c
Prateek <Prateekrajgrd74@gmail.com>
2023-01-06 09:24:30 +0530

    listmethods.py

---
+++ listmethods.py
@@ -0,0 +1,17 @@
+l = [11,45,1,2,4,6,1]
+print(l)
+
+# l.append(7)                    #it adds the elelment at the end of the list
+# l.sort()                       #print the elements in ascending order
+# l.sort(reverse=True)           #print the elements in descending order 
+# l.reverse()                    #print the list in reverse order
+# print(l.index(1))              #print the index of given element
+# print(l.count(1))              #print the count of the given element
+# m = l.copy()                   #it will make a copy of the list
+#l.insert(1,899)                 #it will insert the element at the given index
+
+m = [900,100,1100]
+k = l + m
+print(k)
+# l.extend(m)
+print(l)
