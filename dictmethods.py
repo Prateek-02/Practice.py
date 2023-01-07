96d22ee78a471c11cf26314c9f58fa99e6105dc4
Prateek <Prateekrajgrd74@gmail.com>
2023-01-07 08:22:06 +0530

    dictmethods.py

---
+++ dictmethods.py
@@ -0,0 +1,7 @@
+ep1 = {122: 45, 123: 89, 567: 69, 670: 69}
+ep2 = {222: 67, 566: 90}
+
+# ep1.update(ep2)                #it will uppdate the 1st dictionary with the key and values of 2nd dictionary
+#ep1.clear()                     #it will clear the dictionary
+ep1.popitem()
+print(ep1)
