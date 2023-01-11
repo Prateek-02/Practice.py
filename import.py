42bec36a303117b54e4da248d2724c8119f0d350
Prateek <Prateekrajgrd74@gmail.com>
2023-01-11 21:12:11 +0530

    import.py

---
+++ import.py
@@ -0,0 +1,14 @@
+import math
+res = math.sqrt(81)
+print(res)
+
+from math import sqrt,pi
+res = sqrt(9) * pi
+print(res)
+
+import math as m
+res = m.sqrt(9)
+print(res)
+
+import math
+print(dir(math))
