dffdeef105c5e3a2c34d952a5d170756b6246253
Prateek <Prateekrajgrd74@gmail.com>
2023-01-12 21:41:54 +0530

    oops1.py

---
+++ oops1.py
@@ -0,0 +1,16 @@
+class Student:
+    pass
+
+harry = Student()
+larry = Student()
+
+
+harry.name = "harry"
+harry.std = 12
+harry.sec = 1
+larry.name = "larry"
+larry.std = 10
+larry.sec = 2
+
+print(harry.name,harry.std,harry.sec)
+print(larry.name,larry.std,larry.sec)
